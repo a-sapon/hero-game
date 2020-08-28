c7fdda07c7f254c901ca546428750650c88f54c3
a-sapon <sapon.anya@gmail.com>
2020-08-28 23:13:16 +0300

    add choose fighter table

--- src/components/mc_choose_hero/ChooseHero.js
+++ src/components/mc_choose_hero/ChooseHero.js
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import images from '../assets/images.json';
 import styles from './ChooseHero.module.css';
 
-console.log(images);
+const ChooseHero = () => {
+  const [activeId, setActiveId] = useState(10);
+  const currObj = images.find(item => item.id === String(activeId));
 
-const ChooseHero = () => (
-  <div className={styles.container}>
-    <h1 className={styles.title}>SELECT YOUR FIGHTER</h1>
-    <ul className={styles.fighters}>
-      {images.map((img) => (
-        <li key={img.id} className={styles.fighter}>
-          <img src={img.avatar} alt={img.name}></img>
-        </li>
-      ))}
-      <img src={images[0].gif} className={styles.gif}></img>
-    </ul>
-  </div>
-);
+  useEffect(() => {
+    const elWithActiveClass = document.querySelector(`.${styles.active}`);
+    if (elWithActiveClass) elWithActiveClass.classList.remove(styles.active);
+    const currentEl = document.getElementById(String(activeId));
+    currentEl.classList.add(styles.active);
+  }, [activeId]);
+
+  useEffect(() => {
+    window.addEventListener('keydown', onKeyPressed);
+    return () => {
+      window.removeEventListener('keydown', onKeyPressed);
+    };
+  });
+
+  function onKeyPressed(e) {
+    const lastEl = +images[images.length - 1].id;
+    const firstEl = +images[0].id;
+    if (e.key === 'ArrowRight') {
+      if (activeId === lastEl) {setActiveId(firstEl)}
+      else if (activeId === 16) {setActiveId(20)}
+      else if (activeId === 26) {setActiveId(31)}
+      else if (activeId === 35) {setActiveId(41)}
+      else {setActiveId((prev) => (prev += 1))}
+    }
+    if (e.key === 'ArrowLeft') {
+      if (activeId === firstEl) {setActiveId(lastEl)}
+      else if (activeId === 20 ) {setActiveId(16)}
+      else if (activeId === 31) {setActiveId(26)}
+      else if (activeId === 41) {setActiveId(35)}
+      else {setActiveId((prev) => (prev -= 1))}
+    }
+    if (e.key === 'ArrowUp') {
+      if (activeId < 20) {return}
+      setActiveId((prev) => (prev -= 10));
+    }
+    if (e.key === 'ArrowDown') {
+      if (activeId > 40 ) {return}
+      else if (activeId === 20) {setActiveId((prev) => (prev += 1))}
+      else if (activeId === 26) {setActiveId((prev) => (prev -= 1))}
+      setActiveId((prev) => (prev += 10));
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>SELECT YOUR FIGHTER</h1>
+      <ul className={styles.fighters}>
+        {images.map((img) => (
+          <li id={img.id} key={img.id} data-disabled={img.disabled && 'disabled'} className={styles.fighter}>
+            <img src={img.avatar} alt={img.name}></img>
+          </li>
+        ))}
+        <img src={currObj.gif} className={styles.gif}></img>
+      </ul>
+    </div>
+  );
+};
 
 export default ChooseHero;
